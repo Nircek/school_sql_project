f5a5f05165ad2fe7ed079c922ee9b0722d9f7e2d
Marcin Zepp <nircek-2103@protonmail.com>
2025-01-24 23:50:01 +0100

    feat: Delete tests

--- run.sh
+++ run.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-set -e
-set -o pipefail
-
-kill_jobs_on_error() {
-    jobs -p | xargs -r kill
-    wait
-    exit 1
-}
-
-trap 'kill_jobs_on_error' ERR
-
 
 [ ! -f .venv/bin/activate ] && {
     [ ! -f virtualenv.pyz ] && wget https://bootstrap.pypa.io/virtualenv.pyz;
@@ -26,14 +15,4 @@ echo "PORT: $PORT"
 . .venv/bin/activate
 cd app
 
-[ "$2" = "run" ] && uvicorn main:APP --host 0.0.0.0 --port "$PORT" --env-file .env
-[ "$2" = "run" ] && exit 0
-
-echo "Running tests..."
-uvicorn main:APP --host 0.0.0.0 --port "$PORT" --env-file .env &
-sleep 2
-python3 test.py "$PORT"
-kill %1
-wait
-echo "Tests passed."
-echo "Now you can run the app with: \`$0 $PORT run\`."
+uvicorn main:APP --host 0.0.0.0 --port "$PORT" --env-file .env
